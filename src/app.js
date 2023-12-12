4cb31d1475e673b3006d7d331c952b750633847a
Jvonte Walker <107204895+ScRubbishJay@users.noreply.github.com>
2023-12-12 17:54:14 +0000

    app.js for loops

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "daddy"];
+let domain = [".com", ".net", ".us", ".gov"];
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let d = 0; d < domain.length; d++) {
+        console.log(`${pronoun[p]}${adj[a]}${noun[n]}${domain[d]}`);
+      }
+    }
+  }
+}
